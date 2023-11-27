aa36a4e891a11af7d094d197bc39e5194a745961
pascaloseko <pascal.ondieki21@gmail.com>
2023-11-27 13:19:41 +0300

    add prod link

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://travel-planning-app-client.vercel.app"
 ]  # Add the origins of your frontend applications
 app.add_middleware(
     CORSMiddleware,
